3d59f523244b9a475b79b35d81c7a303be46a17a
Sunnyyoung <iSunnyyoung@gmail.com>
2021-09-17 12:58:26 +0800

    Update wechattweak-cli cask
    Version bump to 1.2

--- Casks/wechattweak-cli.rb
+++ Casks/wechattweak-cli.rb
@@ -1,6 +1,6 @@
 cask "wechattweak-cli" do
-  version "1.1"
-  sha256 "4b686e3f75f40fcc60edfd7b504cac970f229cc623818bcf771642ac645422ce"
+  version "1.2"
+  sha256 "c5b1eccc208a380b62ff67cf4df976614d3c39322f79b9c3c3155cc9b3e4ae69"
   url "https://github.com/Sunnyyoung/WeChatTweak-CLI/releases/download/#{version}/wechattweak-cli"
 
   name "WeChatTweak-CLI"
